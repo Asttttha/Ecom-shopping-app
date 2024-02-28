7eb0f49c11aaa8897b8070c366ee592ecb3c7df6
Astha Samaddar <95849912+Asttttha@users.noreply.github.com>
2024-02-28 19:17:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import NavBar from "./Components/Navbar/NavBar";
 import Home from "./Components/Home/Home";
 import Main from "./Components/Main/Main";
 import Footer from "./Components/Footer/Footer";
-import Hero from "./Components/HeroSlider/Hero";
+// import Hero from "./Components/HeroSlider/Hero";
 
 
 function App() {
